a5c2d9247889fef78d4cc7c5f36a07e7526bc374
kgujral2612 <kgujral@pdx.edu>
2022-05-18 00:39:21 -0700

    Fixed noise issue, added triangle oscillator

--- Oscillators/Oscillator.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class WaveOscillator(ABC):
-
-    @abstractmethod
-    def get_wave(self, frequency, duration=1, phase=0, sample_rate=44100):
-        pass
--- Oscillators/Sine.py
+++ Oscillators/Sine.py
@@ -1,14 +1,22 @@
+import math
+import itertools
 import numpy as np
-from Oscillators import Oscillator
 
 
-class Sine_Oscillator(Oscillator.WaveOscillator):
+class Sine_Oscillator:
 
-    def get_wave(self, frequency, duration, phase, sample_rate=44100):
-        sample_count = int(sample_rate * duration)
-        x = np.linspace(0, duration, num=sample_count, endpoint=False)
-        y = np.sin((2 * np.pi * frequency * x) + phase)
-        audio_sin = y * (2 ** 15 // 4) / np.max(np.abs(y))
-        audio_sin = audio_sin.astype(np.int16)
-        return audio_sin, sample_rate
+    def sin_osc(self, freq, amp, phase, sample_rate):
+        phase = (phase / 360) * 2 * math.pi
+        increment = (2 * math.pi * freq) / sample_rate
+        return (math.sin(phase + v) * amp for v in itertools.count(start=0, step=increment))
+
+    def get_iterator(self, iterator, n):
+        return [next(iterator) for i in range(n)]
+
+    def get_wave(self, freq, amp=1, phase=0, sample_rate=44100):
+        osc = self.sin_osc(freq, amp, phase, sample_rate)
+        y = np.array(self.get_iterator(osc, sample_rate), dtype=np.float32)
+        audio = y * (2 ** 15 // 2) / np.max(np.abs(y))  # ensuring the correct range for the amplitude
+        return audio.astype(np.int16)
+
 
