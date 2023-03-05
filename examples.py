1c2c75531dfb33d9b73eda5bdf2ff89e8c295df4
kgujral2612 <kgujral@pdx.edu>
2023-03-04 22:20:13 -0800

    fixed linting errors in examples.py and interface_handler.py

--- examples.py
+++ examples.py
@@ -1,19 +1,22 @@
-from interface_handler import *
+""" This module contains some examples displaying the features by the project
+
+***** Options *****
+Wave Type:  Sine (1), Square (2), Sawtooth (3), Triangle (4),
+             Oboe (5), Flute(6), Bell (7), Violin (8)
+Envelopes:  Trapezoidal (1), ADSR (2)
+Effects:    White Noise (1), Environmental Noise (2), Time Stretch (3),
+            Pitch Scaling (4), Inverse Polarity (5), Random Gain (6)
+Filters:    Low-Pass (1)
+============================================================================================
+"""
+from interface_handler import play_notes, play_midi
 
 
-# ***** Options *****
-# Wave Type: Sine (1), Square (2), Sawtooth (3), Triangle (4), Oboe (5), Flute(6), Bell (7), Violin (8)
-
-# Envelopes: Trapezoidal (1), ADSR (2)
-
-# Effects: White Noise (1), Environmental Noise (2), Time Stretch (3),
-# Pitch Scaling (4), Inverse Polarity (5), Random Gain (6)
-
-# Filters: Low-Pass (1)
-# ============================================================================================
-
-# Mode-A
-def eg_1():
+def mode_a_example():
+    """ A sample code that runs in mode-a. 
+        The user must input a set of notes and beats,
+        and the program plays those notes.
+    """
     print("\nEg-1: Playing silent night on an oboe")
     notes = ["A4", "B4", "A4", "F#4", "A4", "B4", "A4", "F#4",
              "E5", "E5", "C#5", "D5", "D5", "A4",
@@ -34,7 +37,10 @@ def eg_1():
     play_notes(notes=notes, beats=beats, bpm=bpm, wave_type=5)
 
 
-def eg_2():
+def effects_example():
+    """ A sample code that demonstrates pitch scaling 
+        and time stretch effects
+    """
     print("\nEg-2: Playing the bell")
     notes = ["C5", "D5", "C5"]
     beats = [1, 2, 1]
@@ -49,21 +55,29 @@ def eg_2():
 
 
 # Mode-B
-def eg_3():
+def sawtooth_example():
+    """ A sample code that demonstrates Sawtooth Wave
+        created by the program
+    """
     print("\nEg-3: Playing Viva La Vida with Sawtooth Wave")
     path = "Files/viva_la_vida.mid"
     play_midi(path, wave_type=3)
 
 
-def eg_4():
+def violin_example():
+    """ A sample code that demonstrates Violin sound
+        created by the program
+    """
     print("\nEg-4: Playing Harry Potter Theme Song on a violin")
     path = "Files/harry_potter.mid"
     play_midi(path, wave_type=8)
 
 
-def eg_5():
+def flute_example():
+    """ A sample code that demonstrates flute sound
+        created by the program 
+    """
     print("\nEg-5: Playing notes on a flute")
-    path = "Files/twinkle_twinkle.mid"
     notes = ["G4", "E4", "E4", "D4", "C4", "D4", "E4", "G4", "D5", "D5", "C5", "D5", "D5", "E5"
              ]
     beats = [1, 1, 1, 3, 1, 1, 2,
@@ -71,16 +85,19 @@ def eg_5():
     play_notes(notes=notes, beats=beats, wave_type=6)
 
 
-def eg_6():
+def triangle_example():
+    """ A sample code that demonstrates triangle wave
+        created by the program 
+    """
     print("Playing Wii Mii Channel Song on a triangle wave")
     path = "Files/wii_mii.mid"
     play_midi(path, wave_type=4)
 
 
 if __name__ == "__main__":
-    eg_1()
-    #eg_2()
-    #eg_3()
-    #eg_4()
-    #eg_5()
-    #eg_6()
+    mode_a_example()
+    effects_example()
+    sawtooth_example()
+    violin_example()
+    flute_example()
+    triangle_example()
--- interface_handler.py
+++ interface_handler.py
@@ -1,65 +1,78 @@
-#
-# This file handles user input
-# *****  Modes  *****
-# A) Notes + Tempo, B) Midi Path, C) Aleatoric
+"""
+This file handles user input
+*****  Modes  *****
+A) Notes + Tempo, B) Midi Path, C) Aleatoric
 ###########################################################################
-# ***** Options *****
-# Wave Type: Sine (1), Square (2), Sawtooth (3), Triangle (4), Oboe (5), Flute(6), Bell (7), Violin (8)
-
-# Envelopes: Trapezoidal (1), ADSR (2)
-
-# Effects: White Noise (1), Environmental Noise (2), Time Stretch (3),
-# Pitch Scaling (4), Inverse Polarity (5), Random Gain (6)
-
-# Filters: Low-Pass (1)
+***** Options *****
+Wave Type:  Sine (1), Square (2), Sawtooth (3), 
+            Triangle (4), Oboe (5), Flute(6), Bell (7), Violin (8)
+Envelopes:  Trapezoidal (1), ADSR (2)
+Effects:    White Noise (1), Environmental Noise (2), Time Stretch (3),
+            Pitch Scaling (4), Inverse Polarity (5), Random Gain (6)
+Filters:    Low-Pass (1)
 ###########################################################################
+"""
+import random
 import numpy as np
-
-from helpers import midi_helper
-from helpers.pitch_helper import *
-from additive_synthesis import wave_table
-from data_augmentation.filters import *
-from data_augmentation.effects import *
 import simpleaudio as sa
 from scipy.io import wavfile
+from helpers import midi_helper
+from helpers.pitch_helper import get_freq_from_name, get_num_from_name, get_freq_from_num
+from additive_synthesis import wave_table
+from data_augmentation.filters import low_pass_filter
+from data_augmentation.effects import white_noise, env_noise
+from data_augmentation.effects import time_stretch, pitch_scaling, inverse_pol, random_gain
 
 
 def create_synth_file_path(path, wave_type, envelope, effect, filter_type):
-    synth_wave_path = "Files/wav files/" + path[6: len(path)-4] + "-" + str(wave_type)
+    """ a utility method to create a file path where the output is saved """
+    synth_wave_path = "Files/wav files/" + \
+        path[6: len(path)-4] + "-" + str(wave_type)
     synth_wave_path += ("-" + str(envelope)) if envelope is not None else ""
     synth_wave_path += ("-" + str(effect)) if effect is not None else ""
-    synth_wave_path += ("-" + str(filter_type)) if filter_type is not None else ""
+    synth_wave_path += ("-" + str(filter_type)
+                        ) if filter_type is not None else ""
     synth_wave_path += "-synth.wav"
     return synth_wave_path
 
 
 def apply_filter(audio, filter_type):
+    """ applies filter to the given audio
+        currently, there is only 1 filter, i.e low-pass
+    """
     if filter_type == 1:  # low-pass
         return low_pass_filter(audio)
     return audio
 
 
 def apply_effect(audio, effect_type):
+    """ applies effects to the given audio
+        white noise (1), environmental noise (2),
+        time stretch (3), pitch scaling (4) ,
+        inverse_pol (5), random gain (6)
+    """
     if effect_type == 1:
         return white_noise(audio)
-    elif effect_type == 2:
+    if effect_type == 2:
         return env_noise(audio)
-    elif effect_type == 3:
+    if effect_type == 3:
         return time_stretch(audio)
-    elif effect_type == 4:
+    if effect_type == 4:
         return pitch_scaling(audio)
-    elif effect_type == 5:
+    if effect_type == 5:
         return inverse_pol(audio)
-    elif effect_type == 6:
+    if effect_type == 6:
         return random_gain(audio)
 
 
-# Mode-A: Notes
-def play_notes(notes, beats=None, bpm=120, wave_type=1, envelope=None, effect=None, filter_type=None):
+def play_notes(notes, beats=None, bpm=120, wave_type=1, 
+               envelope=None, effect=None, filter_type=None):
+    """ plays a given set of notes and beats (mode-a)"""
     if not notes or len(notes) == 0:
         return None
     if not wave_type:
         wave_type = 1
+
     # Step-1: Calculate frequencies from notes
     beat_duration = 60 / bpm
     if not beats:
@@ -68,19 +81,23 @@ def play_notes(notes, beats=None, bpm=120, wave_type=1, envelope=None, effect=No
 
     curr = 0
     duration_freq_dict = {}
-    for i in range(len(beats)):
-        val = [(get_freq_from_name(notes[i]), beats[i])]
+
+    for i, beat in enumerate(beats):
+        val = [(get_freq_from_name(notes[i]), beat)]
         duration_freq_dict[curr] = val
-        curr += beats[i]
+        curr += beat
 
     duration = curr
 
     # Step-2: create the audio
     rate = 48000
-    synth_wave_path = create_synth_file_path("Files/play_notes.mid", wave_type, envelope, effect, filter_type)
-    audio = wave_table.WaveTable(duration_freq_dict, duration, wave_type, envelope, rate).tabulate()
+    synth_wave_path = create_synth_file_path(
+        "Files/play_notes.mid", wave_type, envelope, effect, filter_type)
+    audio = wave_table.WaveTable(
+        duration_freq_dict, duration, wave_type, envelope, rate).tabulate()
     if not wave_type:
         wave_type = 1
+
     # Step-3: apply filters
     if filter_type:
         audio = apply_filter(audio, filter_type)
@@ -97,12 +114,13 @@ def play_notes(notes, beats=None, bpm=120, wave_type=1, envelope=None, effect=No
     wavfile.write(synth_wave_path, rate, audio)
 
 
-# Mode-B: Midi File
 def play_midi(path=None, wave_type=1, envelope=None, effect=None, filter_type=None):
+    """ reads and plays notes from a midi file """
     if path is None:
         return None
     if not wave_type:
         wave_type = 1
+
     # Step-1: read midi file
     result = midi_helper.Midi_Helper.input_midifile(path)
     if result.length > 0:
@@ -110,16 +128,17 @@ def play_midi(path=None, wave_type=1, envelope=None, effect=None, filter_type=No
         rate = 48000  # default rate
         val = 0
         key = 0
-        for k, v in midi_info_list.items():
-            key = k
-            val = v
-        x = key
-        y = val[-1]
-        duration = x + y[1]
+        for m_key, m_val in midi_info_list.items():
+            key = m_key
+            val = m_val
+        part_dur = val[-1]
+        duration = key + part_dur[1]
 
         # Step-2: create the audio
-        synth_wave_path = create_synth_file_path(path, wave_type, envelope, effect, filter_type)
-        audio = wave_table.WaveTable(midi_info_list, duration, wave_type, envelope, rate).tabulate()
+        synth_wave_path = create_synth_file_path(
+            path, wave_type, envelope, effect, filter_type)
+        audio = wave_table.WaveTable(
+            midi_info_list, duration, wave_type, envelope, rate).tabulate()
 
         # Step-3: apply filters
         if filter_type:
@@ -137,38 +156,27 @@ def play_midi(path=None, wave_type=1, envelope=None, effect=None, filter_type=No
         wavfile.write(synth_wave_path, rate, audio)
 
 
-# Mode-C: Aleatoric
 def play_aleatoric(root_note, bpm, beats,  wave_type=1, envelope=None):
+    """ plays given notes in an aleatoric mode """
     duration = 60/bpm
     rate = 48000
     root = get_num_from_name(root_note)
     key_arr = [root+2, root+4, root+5, root+7, root+9, root+11, root+12]
     root_freq = get_freq_from_name(root_note)
     duration_freq_dict = {}
-    duration_freq_dict[0]=[(root_freq,duration)]
-    curr = duration
+    duration_freq_dict[0] = [(root_freq, duration)]
     final = []
     while True:
-        root_data = wave_table.WaveTable(duration_freq_dict, duration, wave_type, envelope, rate).tabulate()
+        root_data = wave_table.WaveTable(
+            duration_freq_dict, duration, wave_type, envelope, rate).tabulate()
         final = root_data
         for i in range(beats-1):
             note = random.choice(key_arr)
             note_freq = get_freq_from_num(note)
-            duration_freq_dict[0]=[(note_freq,duration)]
-            note_data = wave_table.WaveTable(duration_freq_dict, duration, wave_type, envelope, rate).tabulate()
+            duration_freq_dict[0] = [(note_freq, duration)]
+            note_data = wave_table.WaveTable(
+                duration_freq_dict, duration, wave_type, envelope, rate).tabulate()
             final = np.concatenate((final, note_data), axis=None)
         play_obj = sa.play_buffer(final, 1, 2, 48000)
         play_obj.wait_done()
     return None
-
-
-
-
-
-
-
-
-
-
-
-
